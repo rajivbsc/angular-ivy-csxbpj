09c70bfb961bb6889dbf833c94fc5921c38de85f
rajivbsc <rajivbsc@gmail.com>
2020-07-15 20:30:24 +0530

    1 file modified, 2 added

---
+++ jasmine-standalone-3.5.0/spec/HelloSpec.js
@@ -0,0 +1,5 @@
+describe("Hello",function(){
+  it("should return Unit Test",function(){
+    expect(Display()).toEqual("Unit Test");
+  })
+})
---
+++ jasmine-standalone-3.5.0/src/Hello.js
@@ -0,0 +1,5 @@
+
+function Display()
+{
+  return "Unit Test";
+}

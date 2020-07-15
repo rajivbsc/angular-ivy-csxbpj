26e091c6fc9a2b223d5275553596c09de723e662
rajivbsc <rajivbsc@gmail.com>
2020-07-15 11:04:29 +0530

    2 files modified

--- src/app/sample.service.ts
+++ src/app/sample.service.ts
@@ -6,12 +6,11 @@ export class TestService
 {
   constructor(private http: HttpClient)
   {
-
   }
   getValues()
   {
-    debugger;
+    
     console.log("Test Files Read by Http");
-    return this.http.get("https://github.com/angular-ivy-csxbpj/app/Test.txt");
+    return this.http.get("https://stackblitz.com/edit/angular-ivy-csxbpj?file=%2fsrc%2fapp%2fTest.txt");
   }
 }
